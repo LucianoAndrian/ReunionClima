15eab6ff9972974e307dce617722d1393d7f543d
Luciano Andrian <44793625+LucianoAndrian@users.noreply.github.com>
2024-09-04 17:35:05 -0300

    upd 8/2024
    Algunas dependencias que pueden no estar instaladas dependiendo la distro de linux.

--- set_up/set_up_varios.sh
+++ set_up/set_up_varios.sh
@@ -31,3 +31,33 @@ else
     echo "Instalando CutyCapt..."
     sudo apt install cutycapt
 fi
+
+
+# Verificar e instalar qpdf
+if command -v qpdf &> /dev/null; then
+    echo "qpdf está instalado."
+else
+    echo "qpdf no está instalado."
+    echo "Instalando qpdf..."
+    sudo apt install qpdf
+fi
+
+# Verificar e instalar pdfcrop
+if command -v pdfcrop &> /dev/null; then
+    echo "pdfcrop está instalado."
+else
+    echo "pdfcrop no está instalado."
+    echo "Instalando texlive-extra-utils, puede tardar..."
+    sudo apt install texlive-extra-utils
+fi
+
+# Verificar e instalar ncal
+if command -v ncal &> /dev/null; then
+    echo "ncal está instalado."
+else
+    echo "ncal no está instalado."
+    echo "Instalando ncal..."
+    sudo apt install ncal
+fi
+
+echo "Done"
